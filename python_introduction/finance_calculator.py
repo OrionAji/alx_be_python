d2a8482bada856fa3e672d35b57b78fdf217e2fb
Gamaliel Aji <ajigamaliel30@gmail.com>
2025-11-06 14:15:26 +0100

    ADd advanced project: Finance calculator.

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+monthly_income = input("Enter your monthly income: ")
+total_expenses = input("Enter your total monthly expenses: ")
+
+monthly_savings = int(monthly_income) - int(total_expenses)
+print(f"Your monthly savings are: ${monthly_savings}")
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")
