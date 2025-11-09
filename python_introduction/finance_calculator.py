b4330fd4b69d7ef45da32f50d9a61aea8b4257e8
Gamaliel Aji <ajigamaliel30@gmail.com>
2025-11-09 20:27:11 +0100

    Made my final correction to code: Total_expenses changed to Monthly_expenses.

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,8 @@
 monthly_income = input("Enter your monthly income: ")
-total_expenses = input("Enter your total monthly expenses: ")
+monthly_expenses = input("Enter your total monthly expenses: ")
 
 monthly_savings = float(monthly_income) - float(total_expenses)
+
 print(f"Your monthly savings are: ${monthly_savings}")
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
