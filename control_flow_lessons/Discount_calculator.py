15ef2bff29138e772854b52d54d6943ae1c51aec
Gamaliel Aji <ajigamaliel30@gmail.com>
2025-11-10 13:56:09 +0100

    Further lessons on Control flow.

--- control_flow_lessons/Discount_calculator.py
+++ control_flow_lessons/Discount_calculator.py
@@ -1,6 +1,6 @@
 purchase_amount = float(input("Enter the purchase amount: "))
 
-if purchase_amount >= 1000;
+if purchase_amount >= 1000:
     discount = 0.1                          # 10% of total purchase
 elif purchase_amount >= 500:
     discount = 0.05                         # 5% of total purchase
---
+++ control_flow_lessons/Mad_Libs.py
@@ -0,0 +1,6 @@
+adjective = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+adjective3 = input("Enter one more adjective: ")
+adjective4 = input("Enter a final adjective: ")
+
+print(f"On a beautiful {adjective} day, I went to the zoo.\nI saw a funny {adjective2} monkey swinging from the trees. \nThen, I spotted a majestic {adjective3} lion lounging in the sun. \nWhat a wild and {adjective4} experience!")
