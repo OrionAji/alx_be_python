8bb6f3cb80f156fa7d9be44f7b1d23b2ba0fa611
Gamaliel Aji <ajigamaliel30@gmail.com>
2025-11-16 09:30:20 +0100

    Correct to satisfy checker

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,8 @@
 user = int(input("Enter the size of the pattern: "))
 
 while user > 0:
-    for i in range(user):
-        print("*" * user)
+    for i in range(user):          # rows
+        for j in range(user):      # columns
+            print("*", end="")     
+        print()                    # move to next line
     break
