1ac5420b20634fefb3eaaca2065a3fa142fd8aca
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2025-12-16 00:13:08 +0100

    Practice for functions

---
+++ functions_exercises/even_odd.py
@@ -0,0 +1,9 @@
+def random_number(number):
+    number%2 == 0
+   
+    if number % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+random_number(8)
+    
