c6e28943fe628e2f509f682b81511e4a0754b0f0
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2025-12-27 00:52:54 +0100

    Corrected day_to_add input statement.

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
     print("Current date and time:", formatted_date)
 
 def calculate_future_date():
-        days_to_add = int(input("Enter number of days: "))
+        days_to_add = int(input("Enter the number of days to add to the current date: "))
         from datetime import datetime, timedelta
         current_date = datetime.now()
         future_date = current_date + timedelta(days=days_to_add)
