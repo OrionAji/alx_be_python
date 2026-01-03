7b8d8acf379713dfa72469f255959b55e83fbf14
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-01-03 16:57:36 +0100

    Adjusted decimal places.

--- programming_paradigms_and_exception_handling/bank_account.py
+++ programming_paradigms_and_exception_handling/bank_account.py
@@ -8,16 +8,16 @@ class BankAccount:
         
         if amount > 0:
             self.account_balance += amount
-            print(f"Deposited: ${amount:.2f}")
+            print(f"Deposited: ${amount:.1f}")
 
     def withdraw(self, amount):
         if 0 < amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount:.2f}")
+            print(f"Withdrew: ${amount:.1f}")
             return True
         else:
             print("Insufficient funds or invalid amount.")
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.1f}")
