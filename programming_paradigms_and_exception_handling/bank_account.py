7fa45c43fa4379bf61883d03b139bce104bbe06f
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-01-03 17:12:45 +0100

    Hope this finally fixes this error !!!!

--- programming_paradigms_and_exception_handling/bank_account.py
+++ programming_paradigms_and_exception_handling/bank_account.py
@@ -3,19 +3,15 @@ class BankAccount:
         self.account_balance = float(initial_balance)
 
     def deposit(self, amount):
-        if amount > 0:
-            self.account_balance += amount
-            # Formatting to .1f to match the [Expected] "67.0"
-            print(f"Deposited: ${amount:.1f}")
+        self.account_balance += amount
+        print(f"Deposited: ${amount:.1f}")
 
     def withdraw(self, amount):
-        if 0 < amount <= self.account_balance:
+        if amount <= self.account_balance:
             self.account_balance -= amount
-            # Formatting to .1f to match the [Expected] "50.0"
             print(f"Withdrew: ${amount:.1f}")
             return True
         else:
-            # Match the exact string "Insufficient funds."
             print("Insufficient funds.")
             return False
 
