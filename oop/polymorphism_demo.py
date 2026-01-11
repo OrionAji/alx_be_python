e0b5b7556219ab602d945a85a4ea57436650a3d5
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-01-11 22:07:55 +0100

    First polymorphism exercise

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        return self.length * self.width
+    
+    print(f"The area of the Rectangle is: {area()}")
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        import math
+        return math.pi * (self.radius ** 2)
+    
+    print(f"The area of the Circle is: {area()}")
